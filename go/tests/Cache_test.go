a34b6e060959fefb6f7264d6cccfe372e53214e4
Sharon Aicler <saichler@gmail.com>
2025-09-12 14:04:29 -0700

    Refactor dcache

--- go/tests/Cache_test.go
+++ go/tests/Cache_test.go
@@ -1,26 +1,31 @@
 package tests
 
 import (
+	"testing"
+
+	"github.com/saichler/l8services/go/services/dcache"
 	. "github.com/saichler/l8test/go/infra/t_resources"
 	. "github.com/saichler/l8test/go/infra/t_service"
-	"github.com/saichler/reflect/go/tests/utils"
-	"github.com/saichler/l8services/go/services/dcache"
 	"github.com/saichler/l8types/go/testtypes"
-	"testing"
+	"github.com/saichler/reflect/go/reflect/introspecting"
+	"github.com/saichler/reflect/go/tests/utils"
 )
 
 func TestCacheListener(t *testing.T) {
-	c := dcache.NewDistributedCache(ServiceName, 0, "TestProto", "", nil, globals)
 	item1 := utils.CreateTestModelInstance(1)
-	globals.Introspector().Inspect(item1)
-	_, err := c.Put(item1.MyString, item1)
+	node, _ := globals.Introspector().Inspect(item1)
+	introspecting.AddPrimaryKeyDecorator(node, "MyString")
+	
+	c := dcache.NewDistributedCache(ServiceName, 0, &testtypes.TestProto{}, nil, nil, globals)
+
+	_, err := c.Put(item1)
 	if err != nil {
 		Log.Fail(t, err.Error())
 		return
 	}
 	item2 := utils.CreateTestModelInstance(1)
 	item2.MyEnum = testtypes.TestEnum_ValueTwo
-	_, err = c.Update(item2.MyString, item2)
+	_, err = c.Patch(item2)
 	if err != nil {
 		Log.Fail(t, err.Error())
 		return
