da2ca9c71509dada89a168194c3935132504fb72
Sharon Aicler <saichler@gmail.com>
2025-12-26 08:39:54 -0800

    Fix web activation

--- go/services/manager/ServiceActivate.go
+++ go/services/manager/ServiceActivate.go
@@ -100,7 +100,10 @@ func (this *ServiceManager) Activate(sla *ifs.ServiceLevelAgreement, vnic ifs.IV
 	}
 
 	webService := handler.WebService()
-
+	if webService == nil {
+		webService = sla.WebService()
+	}
+	
 	if ok && webService != nil {
 		vnic.Resources().Logger().Debug("Sent Webservice multicast for ", sla.ServiceName(), " area ", sla.ServiceArea())
 		vnic.Multicast(ifs.WebService, 0, ifs.POST, webService.Serialize())
